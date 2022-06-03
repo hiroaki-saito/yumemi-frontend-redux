dbee76f4c0d90b5dfaba1078054f0565df3f8271
saito-hiroaki <hiro021312@gmail.com>
2022-06-03 22:06:24 +0900

    [add]Initial commit.

---
+++ work/src/yumemi-frondend-redux/my-app/src/features/contents/Contents.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PrefectureCheckUnit from "./PrefectureCheckUnit";
+
+const Contents = () => {
+  return (
+    <div>
+      <h2>都道府県</h2>
+      <PrefectureCheckUnit></PrefectureCheckUnit>
+    </div>
+  );
+};
+
+export default Contents;
---
+++ work/src/yumemi-frondend-redux/my-app/src/features/contents/PrefectureCheckUnit.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PrefectureCheckUnit = () => {
+  return (
+    <div>
+      <input type="checkbox" name="name" />
+      <label>aaa</label>
+    </div>
+  );
+};
+
+export default PrefectureCheckUnit;
